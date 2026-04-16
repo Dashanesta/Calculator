c7855b1c2e1c98aec6bad9b59a7d766c3bf6e163
Shane Delmar-Gordon <dashanesta@gmail.com>
2026-04-16 19:33:15 -0400

    first commit

---
+++ full_adder.v
@@ -0,0 +1,13 @@
+module full_adder(A, B, cin, sum, cout);
+  input A, B, cin;
+  output sum, cout;
+  
+  wire s1, c1, c2;
+  
+  half_adder ha1 (.A(A), .B(B), .sum(s1), .carry(c1));
+  half_adder ha2 (.A(s1), .B(cin), .sum(sum), .carry(c2));
+  
+  or g1 (cout, c1, c2);
+  
+  
+endmodule
