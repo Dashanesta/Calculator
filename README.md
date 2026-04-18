716706d5fe099ad3074639816a89ea83c0d06242
Shane Delmar-Gordon <dashanesta@gmail.com>
2026-04-18 17:28:36 -0400

    twos comp

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 TO DO:
 
 - adder_16b : shane ✅
-- substractor_16b ✅, 2s comp : shane
+- substractor_16b ✅, 2s comp ✅: shane
+    NOTES:
+        - If using 2s comp for subtraction, use the adder, not the subtractor. This follows the principle that 5 + (-3) = 5 - 3.
+
 - bin_multiplier : Avi
 - bin_dividor, bin_modulus : shane
 - double dabble, rdd : avi
--- test_bench.v
+++ test_bench.v
@@ -1,27 +1,31 @@
 `include "adder_16b.v"
 `include "subtractor_16b.v"
+`include "twos_complement.v"
 
 module testbench;
     reg [15:0] a_add, b_add, a_sub, b_sub;
+    reg [15:0] neg_in;
     wire[15:0] sum, diff;
+    wire[15:0] neg_out;
 
-    adder_16b uut1(.a(a_add),.b(b_add),.sum(sum));
-    subtractor_16b uut2(.a(a_sub),.b(b_sub),.diff(diff));
+    adder_16b adder (.a(a_add),.b(b_add),.sum(sum));
+    subtractor_16b subtractor (.a(a_sub),.b(b_sub),.diff(diff));
+    twos_complement twos (.in(neg_in), .out(neg_out));
 
     initial begin
         
         // set test inputs
-        //a_add = 'd240; b_add = 'd129;
-        a_sub = 'd129; b_sub = 'd30;
+        a_add = 'd240; neg_in = 'd129; #0; b_add = neg_out;
+        //a_sub = 'd129; b_sub = 'd30;
         
         // dump waveform
         $dumpfile("dump.vcd");
         $dumpvars(1);
 
         // display a, b, and sum in both binary and decimal form
-        //$display("%d + %d", a, b);
-        //$monitor("Sum (bin): %16b\nSum (dec): %d", sum, sum);
-        $monitor ("Diff (bin): %16b\nDiff (dec): %d", diff, diff);
+        //$display("%d + %d", a_add, b_add);
+        $monitor("Sum (bin): %16b\nSum (dec): %d", sum, sum);
+        //$monitor ("Diff (bin): %16b\nDiff (dec): %d", diff, diff);
 
     end 
 endmodule
